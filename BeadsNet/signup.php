fd0e446d408c4c5049d16837c3e6352bf8f58d9c
ZikeP <zpeng4@ucsc.edu>
2018-11-11 15:53:34 -0800

    database test

---
+++ BeadsNet/signup.php
@@ -0,0 +1,151 @@
+﻿<!DOCTYPE html>
+<?php
+include ("db_conncet.php")
+$con = mysql_connect("localhost","peter","abc123");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+// some code
+
+?>
+<html>
+<title>signup.html</title>
+<script type="text/javascript">
+function store_account()
+{
+document.getElementById("myHeader").value="Nice day!";
+}
+</script>
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    /* Full-width input fields */
+    input[type=text], input[type=password] {
+        width: 100%;
+        padding: 15px;
+        margin: 5px 0 22px 0;
+        display: inline-block;
+        border: none;
+        background: #f1f1f1;
+    }
+
+        /* Add a background color when the inputs get focus */
+        input[type=text]:focus, input[type=password]:focus {
+            background-color: #ddd;
+            outline: none;
+        }
+
+    /* Set a style for all buttons */
+    button {
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        opacity: 0.9;
+    }
+
+        button:hover {
+            opacity: 1;
+        }
+
+    /* Extra styles for the cancel button */
+    .cancelbtn {
+        padding: 14px 20px;
+        background-color: #f44336;
+    }
+
+    /* Float cancel and signup buttons and add an equal width */
+    .cancelbtn, .signupbtn {
+        float: left;
+        width: 50%;
+    }
+
+
+    /* The Modal (background) */
+    .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: #474e5d;
+        padding-top: 50px;
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+        background-color: #fefefe;
+        margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+    }
+
+    /* Style the horizontal ruler */
+    hr {
+        border: 1px solid #f1f1f1;
+        margin-bottom: 25px;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: #f44336;
+        cursor: pointer;
+    }
+
+    /* Clear floats */
+    .clearfix::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+
+    /* Change styles for cancel button and signup button on extra small screens */
+    @media screen and (max-width: 300px) {
+        .cancelbtn, .signupbtn {
+            width: 100%;
+        }
+    }
+</style>
+<body>
+    <form action="/action_page.php">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        <p>Already have an account? <a href="login.html" style="color: dodgerblue">Sign-in</a> here</p>
+        <hr>
+        <label for="email"><b>Email</b></label>
+        <input id= "email" type="text" placeholder="Enter Email" name="email" required>
+
+        <input id="password" name="password" type="password" pattern="^\S{6,}$" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Must have at least 6 characters' : '');
+        if (this.checkValidity()) form.password_two.pattern = this.value;" placeholder="Password" required>
+
+        <input id="password_two" name="password_two" type="password" pattern="^\S{6,}$" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');
+        " placeholder="Verify Password" required>
+
+        <label>
+            <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+        </label>
+
+      <!--  <p>By creating an account you agree to our <a href="" style="color:dodgerblue">Terms & Privacy</a>.</p> -->
+
+        <div class="clearfix">
+            <button type="button" onclick="location.href='home.html';" class="cancelbtn" >Cancel</button>
+            <button type="submit" class="signupbtn">Sign up</button>
+        </div>
+    </form>
+
+</body>
+</html>
