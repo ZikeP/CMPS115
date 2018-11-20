c34b5223e179cee963c1c768a64e309807d6b497
Jiapei Kuang <jkuang6@ucsc.edu>
2018-11-19 21:02:26 -0800

    Basic search function done

--- BeadsNet/js/script2.js
+++ BeadsNet/js/script2.js
@@ -1,3 +1,5 @@
+//Themoviedatabase api implementation
+//For search movies 
 //api_key: edd37bcd78573bd6c7db2376590199f9
 
 
@@ -57,8 +59,6 @@ function getOutput(result){
 	return output;
 }
 
-
-
 function searchtag(keyword){
 	$('#results').html('');
 	$('#buttons').html('');
@@ -66,21 +66,42 @@ function searchtag(keyword){
 	q = keyword;
 
 	$.get(
-		"https://www.googleapis.com/youtube/v3/search",{
-			part: 'snippet, id',
-			q: q,
-			type: 'video',
-			maxResults: 5,
-		key: 'AIzaSyACKXDzqV-oux04qeZLl2APoutqjWVRmU0'},
+		"https://api.themoviedb.org/3/search/movie?",{
+		api_key: 'edd37bcd78573bd6c7db2376590199f9',
+		query: q},
 		function(data){
 			var nextPageToken = data.nextPageToken;
 			var prevPageToken = data.prevPageToken;
 			
 			console.log(data);
 			
-			$.each(data.items, function(i, item){
-				var output = getOutput(item);
-				
+			$.each(data.results, function(r, result){
+				var output = getOutput(result);
+				$('#results').append(output);
+			});
+		}
+		
+	);
+}
+
+function searchanimetag(keyword){
+	$('#results').html('');
+	$('#buttons').html('');
+
+	q = keyword;
+
+	$.get(
+		"https://api.themoviedb.org/3/search/movie?",{
+		api_key: 'edd37bcd78573bd6c7db2376590199f9',
+		query: q},
+		function(data){
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+			
+			console.log(data);
+			
+			$.each(data.results, function(r, result){
+				var output = getOutput(result);
 				$('#results').append(output);
 			});
 		}
